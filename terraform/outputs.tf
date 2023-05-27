6aed487d85cd591f0d05f173deeac9edea649086
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-27 09:23:07 +0100

    Debugging

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
+output "web_apps" {
+  value = zipmap(values(azurerm_linux_web_app.app)[*].name, values(azurerm_linux_web_app.app)[*].resource_group_name)
+}
+
 output "web_app_names" {
   value = values(azurerm_linux_web_app.app)[*].name
 }
