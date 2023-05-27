98fe10a6548488a439b3e78f6186cacdbe60350d
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-27 16:49:16 +0100

    Why am I doing this?

--- terraform/app-service.tf
+++ terraform/app-service.tf
@@ -38,23 +38,18 @@ resource "azurerm_linux_web_app" "app" {
   }
 }
 
-resource "azapi_resource" "app" {
+resource "azapi_update_resource" "app" {
   for_each = toset(var.locations)
 
   type        = "Microsoft.Web/sites/basicPublishingCredentialsPolicies@2022-09-01"
 
   //parent_id = azurerm_linux_web_app.app[each.value].id
-  resource_id = format("%s/basicPublishingCredentialsPolicies", azurerm_linux_web_app.app[each.value].id)
+  resource_id = format("%s/basicPublishingCredentialsPolicies/scm", azurerm_linux_web_app.app[each.value].id)
   //name = format("%s/basicPublishingCredentialsPolicies", azurerm_linux_web_app.app[each.value].name)
 
   body = jsonencode({
     properties = {
-      ftp = {
-        allow = false
-      },
-      scm = {
-        allow = true
-      }
+      allow = true
     }
   })
 
