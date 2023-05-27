a393e196fc16452e45657d552bb991da2c04e77e
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-27 09:54:08 +0100

    Updating matrix

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,10 @@
 locals {
-  // Create a list containing an object for each web app containing the name and resource group name
   web_apps = [for web_app in azurerm_linux_web_app.app : {
     name = web_app.name
     resource_group_name = web_app.resource_group_name
   }]
 }
 
-
 output "web_apps" {
   value = local.web_apps
-}
-
-output "web_app_names" {
-  value = values(azurerm_linux_web_app.app)[*].name
 }
