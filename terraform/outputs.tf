e55cabe5308e43abe264f663419faf4f8fab6ccf
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-27 09:43:34 +0100

    Asked copilot what the answer should be

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,14 @@
+locals {
+  // Create a list containing an object for each web app containing the name and resource group name
+  web_apps = [for web_app in azurerm_linux_web_app.app : {
+    name = web_app.name
+    resource_group_name = web_app.resource_group_name
+  }]
+}
+
+
 output "web_apps" {
-  value = toset(zipmap(values(azurerm_linux_web_app.app)[*].name, values(azurerm_linux_web_app.app)[*].resource_group_name))
+  value = local.web_apps
 }
 
 output "web_app_names" {
