619b8057e874d00606847deb10e2606cd7233d7f
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-28 07:21:18 +0100

    Fix resource group reference

--- terraform/logging.tf
+++ terraform/logging.tf
@@ -23,8 +23,8 @@ resource "azurerm_application_insights" "ai" {
 
   name = format("ai-%s-%s-%s", random_id.environment_id.hex, var.environment, each.value)
 
-  resource_group_name = azurerm_resource_group.ai[each.value].name
-  location            = azurerm_resource_group.ai[each.value].location
+  resource_group_name = azurerm_resource_group.log[each.value].name
+  location            = azurerm_resource_group.log[each.value].location
 
   workspace_id = azurerm_log_analytics_workspace.law.id
 
